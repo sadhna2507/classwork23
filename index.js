0f8023525eaab627c206f13521dd9347f1b1b23b
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-23 20:56:36 +0530

    change1

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function fibo(first, second, counter){
-    if(counter<=10){
-    console.log(first);
+// function fibo(first, second, counter){
+//     if(counter<=10){
+//     console.log(first);
 
-        let c = first + second;
-        return fibo(second,c,counter+1);
-    }
+//         let c = first + second;
+//         return fibo(second,c,counter+1);
+//     }
     
-}
-fibo(1,1,1);
+// }
+// fibo(1,1,1);
+
+
+
+function factorial(num,fact){
+    if(num>0){
+        fact*=num;
+        return factorial(num-1,fact);
+    }else{
+        return fact;
+    }
+}console.log(factorial(5,1))
